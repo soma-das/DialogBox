0a35446251d94dec0d182f85c897b702368d62f8
soma <soma@129.241.208.92>
2018-02-08 15:57:52 +0100

    Taking user input and putting it in a sentence.
    All files uploaded

---
+++ sd.usingdialogboxprint/src/sd/usingdialogboxprint/printwholesentence/component/Component.java
@@ -0,0 +1,12 @@
+package sd.usingdialogboxprint.printwholesentence.component;
+
+import no.ntnu.item.arctis.runtime.Block;
+
+public class Component extends Block {
+
+	public void nameInSentence(String dialog) {
+		
+		System.out.println("Hi! Jeg heter " + dialog +"." + "\n" + " Hva heter du?");
+	}
+
+}
